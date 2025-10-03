56b4c4d3d6ff7c8394532ee6a650972ff5675888
IrshadCodes7 <irshadmd92236@gmail.com>
2025-10-03 19:16:51 +0530

    Implement majorDisplay and minorDisplay for calculator UI

--- script.js
+++ script.js
@@ -12,17 +12,33 @@ function divide(num1,num2){
 }
 
 function operate(num1,num2,oprator){
+    let ans
     if(oprator=="plus"){
-        add(num1,num2)
+        ans=add(num1,num2)
     }
     if(oprator=="minus"){
-        subtract(num1,num2)
+        ans=subtract(num1,num2)
     }
     if(oprator=="mul"){
-        multiply(num1,num2)
+        ans=multiply(num1,num2)
     }
     if(oprator=="div"){
-        divide(num1,num2)
+        ans=divide(num1,num2)
     }
+    return ans
 }
+
+const majorDisp=document.querySelector(".currInput")
+const minorDisp=document.querySelector(".fullInput")
+
+function majorDisplay(content){
+   majorDisp.textContent=content
+}
+
+function minorDisplay(content){
+    minorDis.textContent=content
+}
+
+
+
 
