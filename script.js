551358e626f412088d001c5424f454379a52aafc
IrshadCodes7 <irshadmd92236@gmail.com>
2025-10-03 19:07:14 +0530

    Add operate function to handle arithmetic operations

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return num1/num2
 }
+
+function operate(num1,num2,oprator){
+    if(oprator=="plus"){
+        add(num1,num2)
+    }
+    if(oprator=="minus"){
+        subtract(num1,num2)
+    }
+    if(oprator=="mul"){
+        multiply(num1,num2)
+    }
+    if(oprator=="div"){
+        divide(num1,num2)
+    }
+}
+
